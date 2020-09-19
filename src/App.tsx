e97a5e70a51b17e7e8cf1b323cd2a0ddfb12c6a8
Marcelo Gomes de Paula <marcelogomesrp@gmail.com>
2020-09-19 12:35:05 -0300

    just for fun

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import capa from './assets/logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={capa} className="App-logo" alt="capa" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Em breve
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
