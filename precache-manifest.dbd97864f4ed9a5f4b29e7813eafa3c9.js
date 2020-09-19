2dd2dfac06c2dc2da3dfc37f497fec51da07d522
Marcelo Gomes de Paula <marcelogomesrp@gmail.com>
2020-09-19 12:18:57 -0300

    Updates

---
+++ precache-manifest.dbd97864f4ed9a5f4b29e7813eafa3c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50b7d2edf640fc4eefb879a0125bb7c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af56382cd2ffeca7d4d6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2d4e944a59e69b621644",
+    "url": "/static/js/2.00af5312.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00af5312.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af56382cd2ffeca7d4d6",
+    "url": "/static/js/main.c5c1baf0.chunk.js"
+  },
+  {
+    "revision": "63aa9a3dd022ef056fac",
+    "url": "/static/js/runtime-main.aaa20a1f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
