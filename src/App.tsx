ae91fa52928c425463e2f9a21ff026e77c646162
Marcelo Gomes <marcelogomesrp@gmail.com>
2020-10-04 02:48:11 -0300

    add toobar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import capa from './assets/logo.svg';
 import './App.css';
+import TooBar from './ToolBar';
 
 function App() {
   return (
     <div className="App">
+      <TooBar />
       <header className="App-header">
         <img src={capa} className="App-logo" alt="capa" />
         <p>
